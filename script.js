bc084876580548c3a4b52e7efad770f8dce47d72
hefren <sanchezhefren@gmail.com>
2022-07-31 10:18:16 -0400

    second commit - optimizing the code

--- script.js
+++ script.js
@@ -7,95 +7,47 @@ const six = document.getElementById('six');
 const rgbText = document.getElementById('color');
 const score = document.getElementById('score');
 const array = [one, two, three, four, five, six];
-
-const random = () =>{
-	let r = Math.floor(Math.random()*6);
-	return array[r];
-}
-
-
-const random_color = ()=>{
-	return Math.floor(Math.random()*256);
-}
+let score_num = 0;
+const random = e =>	Math.floor(Math.random()*e);
 
 const color = ()=>{
-	return `rgb(${random_color()},${random_color()},${random_color()})`;
+	return `rgb(${random(256)},${random(256)},${random(256)})`;
 }
 
 const message = m => {
 	let cover = document.createElement('div');
 	let text;
-	if(!m) {
-		text = document.createTextNode('You Lose');
-	} else {
-		text = document.createTextNode('You Win');
-	}
+	(!m) ? text = document.createTextNode('You Lose') : text = document.createTextNode('You Win');
+	
 	cover.setAttribute('class', 'message');
 	cover.appendChild(text)
 	document.body.appendChild(cover);
 	cover.addEventListener('click', ()=>cover.remove())
 }
 
-
-
 const play = () => {
-	one.style.background = color();
-	two.style.background = color();
-	three.style.background = color();
-	four.style.background = color();
-	five.style.background = color();
-	six.style.background = color();
+	array.forEach(e => e.style.background = color())
+	winner = array[random(6)];
+	rgbText.innerHTML = winner.style.background;
+};
 
-	console.table(
-		{
-			one:  one.style.background,
-			two:  two.style.background,
-			three:  three.style.background,
-			four:  four.style.background,
-			five:  five.style.background,
-			six:  six.style.background
-		}
-	)
-}
-play()
-
-let winner = random();
-rgbText.innerHTML = winner.style.background;
-score.innerHTML = 0;
-
-const asing = a =>{
+const assign = a =>{
 	if (winner === a) {
 		play()
-		winner = random();
-		rgbText.innerHTML = winner.style.background;
-		score.innerHTML -= - 5;
-
+		score_num -= - 4;
 		return	1
 	} else {
-		score.innerHTML -= + 2;
+		score_num -= + 3;
 		return 0
 	}
 }
-const handlers = () => {
-	one.addEventListener('click', () => {
-		message(asing(one))
-	})
-	two.addEventListener('click', () => {
-		message(asing(two))
-	})
-	three.addEventListener('click', () => {
-		message(asing(three))
-	})
-	four.addEventListener('click', () => {
-		message(asing(four))
-	})
-	five.addEventListener('click', () => {
-		message(asing(five))
-	})
-	six.addEventListener('click', () => {
-		message(asing(six))
-	})
 
+const handlers = () =>{
+	array.forEach(e =>{
+		e.addEventListener('click', () => {
+			message(assign(e))
+			score.innerText = `Score: ${score_num}`;
+		})
+	})
 }
-
-handlers();
+handlers();play();
